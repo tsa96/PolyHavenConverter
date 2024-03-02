9621450de2f0c51a697218e568fb0a9252775eaf
tsa96 <tom@trumpetdust.org>
2024-03-02 07:57:58 +0000

    chore: delete PyVMF

--- vmfmaker/debug/ico.py
+++
@@ -1,8 +0,0 @@
-from vmfmaker.obj import *
-
-v = new_vmf()
-
-for solid in obj_to_solids("minecraft.obj", "banana/minecraft", 64):
-    v.add_solids(solid)
-
-v.export("ico.vmf")
--- vmfmaker/debug/window.py
+++
@@ -1,8 +0,0 @@
-from vmfmaker.PyVMF import *
-
-v = new_vmf()
-
-r = SolidGenerator.surf_ramp(Vertex(), 1024, 576, 128, 32, 1)
-v.add_solids(r)
-
-v.export("window_g.vmf")
